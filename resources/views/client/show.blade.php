4920422381ca16790d2cc456e0e45ad26aea56f6
Jorge Luis <jorgeconsalvacion@gmail.com>
2016-08-16 23:38:52 -0500

    feat(client): view client

--- resources/views/client/show.blade.php
+++ resources/views/client/show.blade.php
@@ -7,5 +7,17 @@
 @endsection
 
 @section('content-page')
-{{$client}}
+	<div class="row">
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<div class="x_panel">
+				<div class="x_title">
+					<h2>Cliente <small> {{$client->name}} {{$client->last_name}} </small></h2>
+					<div class="clearfix"></div>
+				</div>
+				<div class="x_content">
+					{{$client}}
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
