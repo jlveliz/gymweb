9086258d20b1b12eca17352e10f6d472078ac7aa
rikuhen <jorgeconsalvacion@gmail.com>
2016-08-18 15:47:12 -0500

    feat(admin): add menu and remove middleware

--- resources/views/partial/nav.blade.php
+++ resources/views/partial/nav.blade.php
@@ -2,7 +2,7 @@
 <div class="menu_section">
 	<h3>Administración</h3>
 	<ul class="nav side-menu">
-		<li class="@if(Request::path() == '/clients') active @endif" "><a href="{{ route('clients.index') }}"><i class="fa fa-user-group"></i> Clientes</span></a></li>
+		<li class="@if(Request::path() == '/clients') active @endif" "><a href="{{ route('clients.index') }}"><i class="fa fa-users"></i> Clientes</span></a></li>
 	</ul>
 </div>
 @endrole
@@ -11,6 +11,8 @@
 	<h3>Configuración</h3>
 	<ul class="nav side-menu">
 		<li class="@if(Request::path() == '/users') active @endif" "><a href="{{ route('users.index') }}"><i class="fa fa-user"></i> Usuarios</span></a></li>
+		<li class="@if(Request::path() == '/roles') active @endif" "><a href="{{ route('roles.index') }}"><i class="fa fa-unlock"></i> Roles</span></a></li>
+		<li class="@if(Request::path() == '/permissions') active @endif" "><a href="{{ route('permissions.index') }}"><i class="fa fa-lock"></i> Permisos</span></a></li>
 	</ul>
 </div>
 @endrole
