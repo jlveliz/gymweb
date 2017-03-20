63fbc165eb485cf36b5938840894e8fcf4c0cd9a
rikuhen <jorgeconsalvacion@gmail.com>
2017-03-20 13:16:44 -0500

    style(login): create a pretty login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.auth')
 @section('content')
- <div class="">
-    <a class="hiddenanchor" id="toregister"></a>
-    <a class="hiddenanchor" id="tologin"></a>
-
-    <div id="wrapper">
-      <div id="login" class="animate form">
+<div id="wrapper">
+      <div id="login" class="animated form bounceIn">
         <section class="login_content">
           <form method="post" action="{{ url('login') }}">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -20,26 +16,26 @@
                   </div>
             @endif
               <div>
-              <input type="text" class="form-control" placeholder="Usuario" name="username" id="username" required="" />
+              <input type="text" class="form-control" placeholder="Usuario" name="username" id="username" required="" autocomplete="false" autofocus />
             </div>
             <div>
               <input type="password" class="form-control" placeholder="Clave" name="password" id="password" required="" />
             </div>
             <div>
-              <button type="submit" class="btn btn-default">Ingresar</button>
-              <label for="remember">
+              <label for="remember" class="pull-left">
               <input type="checkbox" name="remember" id="remember" value="true"> Recordarme
               </label>
+              <button type="submit" class="btn btn-block btn-submit">Ingresar</button>
               {{-- <a class="reset_pass" href="#">Perdió su clave?</a> --}}
             </div>
             <div class="clearfix"></div>
             <div class="separator">
               <div class="clearfix"></div>
               <br />
-              <div>
-                <h1><i class="fa fa-cog" style="font-size: 26px;"></i> GymWeb!</h1>
+              <div class="footer-login">
+                <h4><i class="fa fa-cog"></i> GymWeb!</h4>
 
-                <p>©{{date('Y')}} Todos los derechos reservados. Una aplicación de <b>Novasystems.</b></p>
+                <p class="copyright">©{{date('Y')}} Todos los derechos reservados. Una aplicación de <b>Novasystems.</b></p>
               </div>
             </div>
           </form>
@@ -47,23 +43,5 @@
         </section>
         <!-- content -->
       </div>
-    </div>
-  </div>
-
-@endsection
-@section('js')
-<script type="text/javascript">
-  $(document).ready(function() {
-    $("#login").bgswitcher({
-      images: ["{{ asset('img/login_1.jpg')}}","{{asset('img/login_2.jpg')}}","{{asset('img/login_3.jpg')}}" ],
-      loop:true,
-      effect: "fade", 
-      interval: 5000, 
-      loop: false, 
-      shuffle: false, 
-      duration: 5000, 
-      easing: "swing"    
-    });
-  });
-</script>
+</div>
 @endsection
--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -8,16 +8,30 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('fonts/css/font-awesome.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/animate.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/icheck/flat/green.css') }}">
 	@yield('css')
 </head>
 <body>
 	<div class="login_background">
+		<p>&nbsp;</p>
 		@yield('content')
 		<script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
 		<script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
 		<script src="{{ asset('js/bg_switcher/jquery.bgswitcher.js') }}" type="text/javascript"></script>
+		<script type="text/javascript">
+		  $(document).ready(function() {
+		    $(".login_background").bgswitcher({
+		      images: ["{{ asset('img/login_1.jpg')}}","{{asset('img/login_2.jpg')}}","{{asset('img/login_3.jpg')}}","{{ asset('img/login_4.jpg')}}" ],
+		      loop:true,
+		      effect: "fade", 
+		      interval: 8000, 
+		      shuffle: false, 
+		      duration: 8000, 
+		      easing: "swing"    
+		    });
+		  });
+		</script>
 		@yield('js')
 	</div>
 </body>
